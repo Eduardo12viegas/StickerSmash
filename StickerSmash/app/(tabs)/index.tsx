ac5cbae7b066e039fe79ed3db235f25d340458b4
Eduardo12Viegas <eduardo.viegas@etec.sp.gov.br>
2025-03-10 10:48:40 -0300

    index e Button

---
+++ StickerSmash/app/(tabs)/index.tsx
@@ -0,0 +1,50 @@
+import { View, StyleSheet } from "react-native";
+import ImageViewer from "@/components/ImageViewer";
+import Button from "@/components/Button";
+import * as ImagePicker from "expo-image-picker";
+
+const PlaceholderImage = require("@/assets/images/background-image.png");
+
+export default function Index() {
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync{{
+      mediaTypes: ["images"],
+      allowsEditing: true,
+      quality: 1,
+    }};
+
+  if (!result.canceled) {
+    console.log(result);
+  } else {
+    alert("Você não selecionou nenhuma imagem!");
+  }
+};
+
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="Escolher uma foto" />
+        <Button label="Usar esta foto" />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#25292e",
+    alignItems: "center",
+  },
+  imageContainer: {
+    flex: 1,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: "center",
+  },
+});
